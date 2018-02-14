c156a913a8234c70180dcd6ac0d1417949508178
abush84@github.com <busha@pitt.edu>
2018-02-14 16:04:37 -0500

    solved bug in bml_defaults

--- bml_defaults.m
+++ bml_defaults.m
@@ -5,10 +5,10 @@ function bml_defaults
 BML_FOLDERS = {'signal','annot','sync','io','utils'};
 
 bmlPath = fileparts(mfilename('fullpath')); % get the full path to this function, strip away 'ft_defaults'
-bmlPath = strrep(bmlPath, '\', '\\');
+%bmlPath = strrep(bmlPath, '\', '\\');
 
 for i=1:numel(BML_FOLDERS)
-  i_path = [bmlPath filesep 'signal'];
+  i_path = [bmlPath filesep BML_FOLDERS{i}];
   if isempty(regexp(path, i_path, 'once'))
     addpath(i_path);
   end
