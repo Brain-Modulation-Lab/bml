7b957079a939ad5d990b0e4aa941cd3d347a736c
Bush <BUSHA@pitt.edu>
2018-09-10 17:23:14 -0400

    bml_chunk_sessions now accepts intended chunk duration as 3rd argument

--- sync/bml_chunk_sessions.m
+++ sync/bml_chunk_sessions.m
@@ -1,15 +1,16 @@
-function chunks = bml_chunk_sessions(session, split_time)
+function chunks = bml_chunk_sessions(session, split_time, chunk_duration)
 
 % BML_CHUNK_SESSIONS breaks up sessions into smaller chunks
 %
 % Use as 
 %   chunks = bml_chunk_sessions(session, split_time)
+%   chunks = bml_chunk_sessions(session, [], chunk_duration)
 %
 % session - annot table of sessions or chunks
 % split_time - double: vector of times at which to split the sessions or 
 %           integer < 10 indicating the number of chuncks each session
 %           should be split into
-%
+% chunk_duration - double: desired duration of the resulting chunks
 % returns a chuked version of session
 
 
@@ -17,13 +18,24 @@ session = bml_annot_table(session);
 assert(isempty(bml_annot_overlap(session)),'sessions should not overlap');
 if nargin == 1 
   split_time = [];
-elseif nargin == 2 && length(split_time)==1 && abs(split_time-round(split_time,0))<1e-9 && split_time <= 10
-  n_split = split_time;
-  split_time = [];
-  for s=1:height(session)
-    bp  = linspace(session.starts(s),session.ends(s),n_split+1);
-    split_time = [split_time, bp(2:(end-1))];
-  end
+elseif nargin == 2
+    if length(split_time)==1 && abs(split_time-round(split_time,0))<1e-9 && split_time <= 10
+        n_split = split_time;
+        split_time = [];
+        for s=1:height(session)
+            bp  = linspace(session.starts(s),session.ends(s),n_split+1);
+            split_time = [split_time, bp(2:(end-1))];
+        end
+    end
+elseif nargin == 3 && isempty(split_time)
+	split_time = [];
+    for s=1:height(session)
+        n_split = round(session.duration(s)/chunk_duration);
+        bp  = linspace(session.starts(s),session.ends(s),n_split+1);
+        split_time = [split_time, bp(2:(end-1))];
+    end    
+else
+    warning("incorrect number of arguments");
 end
 
 if ~ismember('session_id',session.Properties.VariableNames)
