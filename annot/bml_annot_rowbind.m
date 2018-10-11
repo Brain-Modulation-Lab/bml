4269e0241848b8519077e39e08be87a0a1115fd3
Bush <BUSHA@pitt.edu>
2018-10-11 10:54:42 -0400

    bml_annot_rowbind can handle empty tables documentation improvements

--- annot/bml_annot_rowbind.m
+++ annot/bml_annot_rowbind.m
@@ -1,13 +1,19 @@
 function annot = bml_annot_rowbind(varargin)
 
-% BML_ANNOT_ROWBIND binds tables by rows, conforming to first table
+% BML_ANNOT_ROWBIND binds tables by rows, conforming to first non-empty table
 %
 % Use as
 %    annot = bml_annot_rowbind(annot1, annot2, annot3, ...)
 
-annot = varargin{1};
-for i=2:numel(varargin)
-  annot = [annot; bml_annot_conform_to(annot, varargin{i})];
+annot = table();
+for i=1:numel(varargin)
+  if ~isempty(varargin{i})
+    if isempty(annot)
+      annot = varargin{i};
+    else
+      annot = [annot; bml_annot_conform_to(annot, varargin{i})];
+    end
+  end
 end
 
 annot.id=[];
