8b42cfc441fac47152e69811c728d0af71422309
Alan Bush <alan.bush@mgh.harvard.edu>
2020-03-03 16:45:50 -0500

    bml_anat_coord2label now alows independent radii and sigma for each electrode

--- anat/bml_anat_coord2label.m
+++ anat/bml_anat_coord2label.m
@@ -11,9 +11,13 @@ function anat_labels = bml_anat_coord2label(cfg, coord)
 % cfg.atlas - name of the atlas to use 
 % cfg.max_assign - integer >= 1, max number of labels assigned to each electrode. Defaults to 1. 
 % cfg.profile - either 'sphere' or 'gaussian'. Defaulst to 'gaussian'.
-% cfg.sigma - float, indicating gaussiand sigma parameter in mm.
+% cfg.sigma - float, indicating gaussian sigma parameter in mm.
+%             if length of sigma equals number of rows in coord, these are
+%             interpreted as electrode specifc sigmas
 % cfg.radius - radius in mm around each electrode coordinate to consider in
-%        label assignation. Defaults to 3mm.
+%             label assignation. Defaults to 3mm.
+%             if length of radius equals number of rows in coord, these are
+%             interpreted as electrode specifc radii
 % cfg.atlas_path - path to volume atlas repository. Defaults to lead-dbs's
 %             icbm 2009b nlin asym labeling folder
 % cfg.keys - table with code to structure key. By default the .txt file 
@@ -49,6 +53,7 @@ lab_col_bn = strsplit(atlas,' ');
 if iscell(lab_col_bn); lab_col_bn = lab_col_bn{1}; end
 lab_col_bn = bml_getopt_single(cfg,'label_column_basename',lab_col_bn);
 
+
 %getting coordinates
 if ~istable(coord); error('coord should be table'); end
 col_idx_x = find(endsWith(coord.Properties.VariableNames,'_x'),1);
@@ -59,6 +64,19 @@ if isempty(col_idx_x) || isempty(col_idx_y) || isempty(col_idx_z)
 end
 XYZ_mm = [coord{:,col_idx_x}, coord{:,col_idx_y}, coord{:,col_idx_z}];
 XYZ_mm = [XYZ_mm ones(size(XYZ_mm,1),1)];
+
+%initializaing radii and sigmas
+if length(sigma)==1
+  sigma = repmat(sigma,height(coord),1);
+elseif length(sigma)~=height(coord)
+  error('sigma should be of length 1 or equal to the number of rows of coord')
+end
+
+if length(radius)==1
+  radius = repmat(radius,height(coord),1);
+elseif length(radius)~=height(coord)
+  error('radius should be of length 1 or equal to the number of rows of coord')
+end
 
 anat_labels = coord;
 if ~return_coords
@@ -76,17 +94,18 @@ for i=1:max_assign
         fprintf('recalculating %s \n',col_Li)
         anat_labels(:,col_Li) = repmat({''},height(anat_labels),1);
     else
-        anat_labels = addvars(anat_labels, repmat({''},height(anat_labels),1),'NewVariableNames',col_Li);             
+        anat_labels = [anat_labels,...
+                       array2table(repmat({''},height(anat_labels),1),'VariableNames',{col_Li})]; 
     end
 
     if ismember(col_Wi,anat_labels.Properties.VariableNames)
         fprintf('recalculating %s \n',col_Wi)
         anat_labels(:,col_Wi) = NaN(height(anat_labels),1);
     else
-        anat_labels = addvars(anat_labels, NaN(height(anat_labels),1),'NewVariableNames',col_Wi);             
+        anat_labels = [anat_labels,...
+                       array2table(repmat({''},height(anat_labels),1),'VariableNames',{col_Wi})];
     end
 end
-
 
 %getting labels nifti
 fname_nii = [atlas_path atlas '.nii'];
@@ -102,34 +121,38 @@ if ~istable(keys)
     keys.Properties.VariableNames = {'id','label'};
 end
     
-%definig built-in profile functions
-if ischar(profile)
-    if strcmp(profile,'sphere')
-        profile = @(v) norm(v)<=radius;
-    elseif strcmp(profile,'gaussian')
-        profile = @(v) exp(-(norm(v)^2)/(2*sigma^2));
-    else
-        error('profile should be a ''sphere'', ''gaussian'' or a function hadle')
-    end
-elseif isa(profile,'function_handle')
-    
-else
-    error('profile should be a ''sphere'', ''gaussian'' or a function hadle')
-end
-
 % Row Column and Slice versors in real world coords
 vR=vx2mm(1:3,1:3)*[1;0;0]; n_vR = norm(vR);
 vC=vx2mm(1:3,1:3)*[0;1;0]; n_vC = norm(vC);
 vS=vx2mm(1:3,1:3)*[0;0;1]; n_vS = norm(vS);
 
-%box 'radii'
-radius_R = ceil(radius./n_vR);
-radius_C = ceil(radius./n_vC);
-radius_S = ceil(radius./n_vS);
+%helper function for matlab 2017b
+sum_all = @(M) sum(M(:)); %use sum(M,'all') in 2018b
 
 %label assignation
 RCS_vx=(vx2mm\XYZ_mm')';
 for e=1:size(RCS_vx,1) %for each electrode
+  
+    %definig built-in profile functions
+    if ischar(profile)
+        if strcmp(profile,'sphere')
+            profile = @(v) norm(v)<=radius(e);
+        elseif strcmp(profile,'gaussian')
+            profile = @(v) exp(-(norm(v)^2)/(2*sigma(e)^2));
+        else
+            error('profile should be a ''sphere'', ''gaussian'' or a function hadle')
+        end
+    elseif isa(profile,'function_handle')
+
+    else
+        error('profile should be a ''sphere'', ''gaussian'' or a function hadle')
+    end
+
+    %box 'radii'
+    radius_R = ceil(radius(e)./n_vR);
+    radius_C = ceil(radius(e)./n_vC);
+    radius_S = ceil(radius(e)./n_vS);
+    
     vx_precise = RCS_vx(e,1:3);
     vx = round(vx_precise);
     if ~any(ismissing(vx))
@@ -148,14 +171,14 @@ for e=1:size(RCS_vx,1) %for each electrode
                 end 
             end
         end
-        w_cube = w_cube./sum(w_cube,'all');
+        w_cube = w_cube./sum_all(w_cube);
         
         % calculating weights for each label
         labs = unique(round(nii.img(range_R,range_C,range_S)));
         labs = setdiff(labs, exclude_labels);
         w = zeros(length(labs),1);
         for l=1:length(labs)
-            w(l) = sum(w_cube .* (round(nii.img(range_R,range_C,range_S)) == labs(l)),'all');
+            w(l) = sum_all(w_cube .* (round(nii.img(range_R,range_C,range_S)) == labs(l)));
         end
         
         [w, sort_idx] = sort(w,'descend');
@@ -167,8 +190,8 @@ for e=1:size(RCS_vx,1) %for each electrode
         
         if ~isempty(labs)
             for i=1:min(max_assign,length(labs))
-                anat_labels(e,[lab_col_bn '_label_' num2str(i)])=labs(i);
-                anat_labels{e,[lab_col_bn '_weight_' num2str(i)]}=w(i);      
+                anat_labels{e,[lab_col_bn '_label_' num2str(i)]}=labs(i);
+                anat_labels{e,[lab_col_bn '_weight_' num2str(i)]}={w(i)};      
             end
         end
         
