b5b29746f8ddb6212968a7bf5c6c468b0762969e
Alan Bush <alan.bush@mgh.harvard.edu>
2020-05-12 09:40:55 -0400

    bml_pad can now deal with multiple trials (not tested)

--- signal/bml_pad.m
+++ signal/bml_pad.m
@@ -20,7 +20,7 @@ if nargin==2
   cfg=raw;
   raw=starts;
   if istable(cfg)
-    if height(cfg) > 1; error('cfg should be a 1 row table'); end
+    %if height(cfg) > 1; error('cfg should be a 1 row table'); end
     cfg=table2struct(cfg);
   end
   starts = bml_getopt(cfg,'starts');
@@ -32,26 +32,50 @@ elseif nargin ~=4
   error('invalid use of bml_pad');
 end
 
-if numel(raw.time) > 1; error('single trial continuous raw required'); end
-nSamples = length(raw.time{1});
+if numel(raw.time) == 1
+  % case single trial. 
+  nSamples = length(raw.time{1});
 
-% tc=[]; 
-% tc.s1=1; tc.s2=length(raw.time{1});
-% tc.t1=raw.time{1}(1); tc.t2=raw.time{1}(end);
-tc = bml_raw2coord(raw);
-[s,e]=bml_crop_idx(tc,starts,ends);
+  % tc=[]; 
+  % tc.s1=1; tc.s2=length(raw.time{1});
+  % tc.t1=raw.time{1}(1); tc.t2=raw.time{1}(end);
+  tc = bml_raw2coord(raw);
+  [s,e]=bml_crop_idx(tc,starts,ends);
 
-padded = bml_crop(raw,starts,ends);
-presamples = max([1-s, 0]);
-postsamples = max([e-nSamples, 0]);
+  padded = bml_crop(raw,starts,ends);
+  presamples = max([1-s, 0]);
+  postsamples = max([e-nSamples, 0]);
 
-if presamples>0 
-  padded.trial{1} = padarray(padded.trial{1},[0 presamples],padval,'pre');
+  if presamples>0 
+    padded.trial{1} = padarray(padded.trial{1},[0 presamples],padval,'pre');
+  end
+  if postsamples > 0
+    padded.trial{1} = padarray(padded.trial{1},[0 postsamples],padval,'post');
+  end
+  padded.time{1} = bml_idx2time(tc,s:e);
+
+  padded.sampleinfo = [s,e];
+
+else % case multiple trials
+	padded = bml_crop(raw,starts,ends);
+  for i=1:numel(raw.time)
+    nSamples = length(raw.time{i});
+    tc = bml_raw2coord(raw, i);
+    [s,e]=bml_crop_idx(tc,starts(i),ends(i));
+    presamples = max([1-s, 0]);
+    postsamples = max([e-nSamples, 0]);
+
+    if presamples>0 
+      padded.trial{i} = padarray(padded.trial{i},[0 presamples],padval,'pre');
+    end
+    if postsamples > 0
+      padded.trial{i} = padarray(padded.trial{i},[0 postsamples],padval,'post');
+    end
+    padded.time{i} = bml_idx2time(tc,s:e);
+
+    padded.sampleinfo(i,:) = [s,e];
+  end
+
 end
-if postsamples > 0
-  padded.trial{1} = padarray(padded.trial{1},[0 postsamples],padval,'post');
-end
-padded.time{1} = bml_idx2time(tc,s:e);
-
-padded.sampleinfo = [s,e];
-
+    
+    
