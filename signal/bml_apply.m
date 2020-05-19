968c6868b08edb5ff6243e300233e7b31d28d956
Alan Bush <alan.bush@mgh.harvard.edu>
2020-05-19 11:28:59 -0400

    added additional arguments to fun in bml_apply

--- signal/bml_apply.m
+++ signal/bml_apply.m
@@ -1,16 +1,23 @@
-function raw = bml_apply(fun, data)
+function raw = bml_apply(fun, data, varargin)
 
 % BML_APPLY applies a function to the data of each trial
 %
 % Use as
 %   raw = bml_apply(fun, data)
+%   raw = bml_apply(fun, data, other, args, for, fun)
 %
 % data - FT_DATAYPE_RAW to which to apply the function fun
 % fun - function to be applyed to the data
 %
 % returns a raw with modified trials. 
 
-for i=1:numel(data.trial)
-  data.trial{i} = fun(data.trial{i});
+if nargin==2
+  for i=1:numel(data.trial)
+    data.trial{i} = fun(data.trial{i});
+  end
+else
+	for i=1:numel(data.trial)
+    data.trial{i} = fun(data.trial{i},varargin{:});
+  end
 end
 raw=data;
